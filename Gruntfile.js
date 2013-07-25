3364bf4f558a86f18495fed2212e248570656001
Hampus Persson <hampus@hampuspersson.se>
2013-07-25 15:23:30 +0200

    Updating copy rules for build process to include all files and folders except those that are specifically excluded

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,132 +1,132 @@
 'use strict';
 module.exports = function(grunt) {
 
-    // load all grunt tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	// load all grunt tasks
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    grunt.initConfig({
+	grunt.initConfig({
 
-        // watch for changes and trigger compass, jshint, uglify and livereload
-        watch: {
-            js: {
-                files: '<%= jshint.all %>',
-                tasks: ['jshint', 'uglify:main']
-            },
-            scss: {
-                  files: 'assets/scss/**/*.scss',
-                  tasks: ['sass:dev']
-            },
-            livereload: {
-                options: { livereload: true },
-                files: ['assets/css/style.css', 'assets/javascripts/*.js', '*.html', '*.php', 'assets/images/**/*.{png,jpg,jpeg,gif,webp,svg}']
-            }
-        },
+		// watch for changes and trigger compass, jshint, uglify and livereload
+		watch: {
+			js: {
+				files: '<%= jshint.all %>',
+				tasks: ['jshint', 'uglify:main']
+			},
+			scss: {
+				  files: 'assets/scss/**/*.scss',
+				  tasks: ['sass:dev']
+			},
+			livereload: {
+				options: { livereload: true },
+				files: ['assets/css/style.css', 'assets/javascripts/*.js', '*.html', '*.php', 'assets/images/**/*.{png,jpg,jpeg,gif,webp,svg}']
+			}
+		},
 
-        sass: {                              // Task
-            dist: {                            // Target
-              options: {
-                style: 'compressed',
-              },
-              files: {                         // Dictionary of files
-                'build/assets/css/style.css': 'assets/scss/style.scss',       // 'destination': 'source'
-              }
-            },
-            dev: {                             // Another target
-              options: {                       // Target options
-                style: 'expanded',
-                sourcemap: 'map/style.css.map'
-              },
-              files: {
-                'assets/css/style.css': 'assets/scss/style.scss',
-              }
-            }
-          },
+		sass: {                              // Task
+			dist: {                            // Target
+			  options: {
+				style: 'compressed',
+			  },
+			  files: {                         // Dictionary of files
+				'build/assets/css/style.css': 'assets/scss/style.scss',       // 'destination': 'source'
+			  }
+			},
+			dev: {                             // Another target
+			  options: {                       // Target options
+				style: 'expanded',
+				sourcemap: 'map/style.css.map'
+			  },
+			  files: {
+				'assets/css/style.css': 'assets/scss/style.scss',
+			  }
+			}
+		  },
 
-        // javascript linting with jshint
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                "force": true
-            },
-            all: [
-                'Gruntfile.js',
-                'assets/javascripts/source/**/*.js'
-            ]
-        },
+		// javascript linting with jshint
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc',
+				"force": true
+			},
+			all: [
+				'Gruntfile.js',
+				'assets/javascripts/source/**/*.js'
+			]
+		},
 
-        // uglify to concat, minify, and make source maps
-        uglify: {
-            main: {
-                options: {
-                    sourceMap: 'assets/javascripts/map/source-map-main.js',
-                    sourceMapRoot: 'assets/javascripts/soruce/',
-                    sourceMappingURL: 'map/source-map-main.js',
-                    mangle: false
-                },
-                files: {
-                    'assets/javascripts/main.min.js': [
-                        'assets/javascripts/source/main.js'
-                    ]
-                }
-            },
+		// uglify to concat, minify, and make source maps
+		uglify: {
+			main: {
+				options: {
+					sourceMap: 'assets/javascripts/map/source-map-main.js',
+					sourceMapRoot: 'assets/javascripts/soruce/',
+					sourceMappingURL: 'map/source-map-main.js',
+					mangle: false
+				},
+				files: {
+					'assets/javascripts/main.min.js': [
+						'assets/javascripts/source/main.js'
+					]
+				}
+			},
 
-            dist: {
-                options: {
-                  mangle: false
-                },
-                files: {
-                    'build/assets/javascripts/main.min.js': [
-                        'assets/javascripts/source/main.js'
-                    ]
-                }
-            }
-        },
+			dist: {
+				options: {
+				  mangle: false
+				},
+				files: {
+					'build/assets/javascripts/main.min.js': [
+						'assets/javascripts/source/main.js'
+					]
+				}
+			}
+		},
 
-        // image optimization
-        imagemin: {
-            dist: {
-                options: {
-                    optimizationLevel: 7,
-                    progressive: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'build/assets/images/',
-                    src: '**/*',
-                    dest: 'build/assets/images/'
-                }]
-            }
-        },
+		// image optimization
+		imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 7,
+					progressive: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'build/assets/images/',
+					src: '**/*',
+					dest: 'build/assets/images/'
+				}]
+			}
+		},
 
-        'ftp-deploy': {
-          dist: {
-            auth: {
-              host: '',
-              port: 21,
-              authKey: 'key1' // key1 is set in .ftppass
-            },
-            src: 'build/',
-            dest: '',
-          }
-        },
+		'ftp-deploy': {
+		  dist: {
+			auth: {
+			  host: '',
+			  port: 21,
+			  authKey: 'key1' // key1 is set in .ftppass
+			},
+			src: 'build/',
+			dest: '',
+		  }
+		},
 
 
-        copy: {
-          dist: {
-            files: [
-              {expand: true, src: [ 'assets/fonts/*' ], dest: 'build/'}, // includes files in path
-              {expand: true, src: [ 'assets/images/**/*' ], dest: 'build/'}, // includes files in path
-              {expand: true, src: [ '*.*', '!Gruntfile.js', '!package.json' ], dest: 'build/'}, // includes files in path
-            ]
-          }
-        }
+		copy: {
+		  dist: {
+			files: [
+				{expand: true, src: [ '**/*', '!assets/**/*', '!node_modules', '!node_modules/**/*' ], dest: 'build/'}, // All files and dir except assets/**/* and node_modules/**/*
+				{expand: true, src: [ 'assets/fonts/*' ], dest: 'build/'},
+				{expand: true, src: [ 'assets/images/**/*' ], dest: 'build/'}
+			]
+		  }
+		}
 
-    });
+	});
 
-    // register task
-    grunt.registerTask('default', ['watch']);
+	// register task
+	grunt.registerTask('default', ['watch']);
 
-    grunt.registerTask('build', [ 'copy:dist', 'sass:dist', 'uglify:dist' ]);
-    grunt.registerTask('upload', [ 'ftp-deploy:dist' ]);
+	grunt.registerTask('build', [ 'copy:dist', 'sass:dist', 'uglify:dist' ]);
+	grunt.registerTask('upload', [ 'ftp-deploy:dist' ]);
 
 };
